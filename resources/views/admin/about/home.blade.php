595cab6dcf1b837a8332b15351d5b0c660257127
vilaphongmk <vilaphong.m@gmail.com>
2022-03-13 15:25:24 +0700

    slide

--- resources/views/admin/about/home.blade.php
+++ resources/views/admin/about/home.blade.php
@@ -1,8 +1,33 @@
 @extends('layouts.admin')
 @section('content')
+<div class="container h-100px ">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{asset('images/images slide/p.jpg')}}" class="d-block w-90 h-100px" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{asset('images/images slide/p.jpg')}}" class="d-block w-90" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{asset('images/images slide/p.jpg')}}" class="d-block w-90" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 
-<h1>Admin Home</h1>
-
-
+</div>
 
 @endsection
