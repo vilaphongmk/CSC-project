050759c922006bf76b214f97361fd7603ed0b0f5
vilaphongmk <vilaphong.m@gmail.com>
2022-03-14 18:32:20 +0700

    slide

--- resources/views/admin/about/home.blade.php
+++ resources/views/admin/about/home.blade.php
@@ -1,42 +1,44 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="co-lg-8 col-12">
-            <div class="container h-100px ">
-                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="{{asset('images/images slide/p.jpg')}}" class="d-block w-90 h-100px" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{asset('images/images slide/p.jpg')}}" class="d-block w-90" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{asset('images/images slide/p.jpg')}}" class="d-block w-90" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-
+<div class="container  box-slider">
+    <div class="item1">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{asset('images/images slide/p.jpg')}}" class="d-block" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{asset('images/images slide/p.jpg')}}" class="d-block " alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{asset('images/images slide/p.jpg')}}" class="d-block" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
 
-        </div>
-        <div class="co-lg-4 col-12">
-            <div class="card ">card</div>
-        </div>
+    </div>
+    <div class="item2">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+    </div>
+    <div class="item3">
     </div>
 </div>
 
